f3c4cac233d98ac21ab42d21b4c90bc34658850e
Joseph <jpaauc1@miumg.edu.gt>
2025-08-12 22:34:37 -0600

    :zap:nuevos cambios2.0

--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("HOLA MUNDO");
+        Console.WriteLine("no se porque no se puede subir esta mierda");
     }
 }
